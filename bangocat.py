8f01bbb7b945e49f19054ba815b3555e120124ad
Raphaël Margueron <raphael.margueron@he-arc.ch>
2020-01-03 16:10:29 +0100

    analyse song for bangocat with librosa + use pygame instead of opencv

--- bangocat.py
+++ bangocat.py
@@ -1,23 +1,87 @@
-import cv2
+import sys
+import time
+import os
 import random
 import argparse
+
+import numpy as np
+import contextlib
+import librosa
+
+with contextlib.redirect_stdout(None):
+    import pygame
+
+
+def get_beat_times(audio_file):
+    y, sr = librosa.load(audio_file)
+    onset_env = librosa.onset.onset_strength(y=y, sr=sr)
+    pulse = librosa.beat.plp(onset_envelope=onset_env, sr=sr)
+    beats_plp = np.flatnonzero(librosa.util.localmax(pulse))
+    times = librosa.times_like(onset_env, sr=sr)
+    beat_times = times[beats_plp]
+    return beat_times
 
 
 def run():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-t", "--time", default=100, type=int, help="Delta T")
+    parser.add_argument(
+        "-f", "--file", default="nyancat.mp3", type=str, help="File")
     parser.add_argument("-s", "--size", default=600, type=int, help="Size")
+    parser.add_argument("-l", "--length", default=200,
+                        type=int, help="Tap length in ms")
 
     args = parser.parse_args()
 
-    img = [cv2.imread("00.png"), cv2.imread("01.png"),
-           cv2.imread("10.png"), cv2.imread("11.png")]
+    if not os.path.exists(args.file):
+        print(f"{args.file} doesn't exist !")
+        parser.print_help()
+        exit()
 
-    img = [cv2.resize(i, (args.size, args.size)) for i in img]
+    size = width, height = args.size, args.size
+    black = (0, 0, 0)
+    tap_length_ms = args.length / 1000
+
+    img0 = pygame.image.load("00.png")
+    imgs = [pygame.image.load("01.png"), pygame.image.load(
+        "10.png"), pygame.image.load("11.png")]
+    img0 = pygame.transform.scale(img0, size)
+    imgs = [pygame.transform.scale(i, size) for i in imgs]
+
+    pygame.mixer.init()
+    pygame.mixer.music.load(args.file)
+
+    beat_times = get_beat_times(args.file)
+
+    pygame.init()
+    pygame.display.set_caption("Bangocat")
+    pygame.display.set_icon(img0)
+    screen = pygame.display.set_mode(size)
+
+    music_start = time.time()
+    pygame.mixer.music.play()
+    last_img = random.choice(imgs)
 
     while True:
-        cv2.imshow("", random.choice(img))
-        cv2.waitKey(args.time)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        screen.fill(black)
+
+        delta_music_start = time.time() - music_start
+        dists = [delta_music_start - i for i in beat_times]
+        min_dists = min(dists, key=lambda x: abs(x))
+        # always after the start of a beat (for tap_length_ms)
+        should_tap = min_dists < tap_length_ms and min_dists > 0
+
+        if should_tap:
+            img = last_img
+        else:
+            img = img0
+            last_img = random.choice(imgs)
+
+        screen.blit(img, (0, 0))
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
